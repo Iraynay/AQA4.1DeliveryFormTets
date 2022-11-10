296bf1e421e2ac9a1df1da4c35d73f598231b88f
Ирина Пащенко <i.m.uglanova@gmail.com>
2022-11-11 02:23:36 +0300

    Add DeliveryFormTest. Not good xpath expressions

---
+++ README.md
@@ -0,0 +1,1 @@
+[![Build status](https://ci.appveyor.com/api/projects/status/sk5beqxtt7623vqw?svg=true)](https://ci.appveyor.com/project/Iraynay/aqa4-1deliveryformtets)
--- src/test/java/DeliveryFormTest.java
+++ src/test/java/DeliveryFormTest.java
@@ -1,2 +1,56 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.conditions.Visible;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+import static com.codeborne.selenide.Selenide.open;
+
 public class DeliveryFormTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    static void setUpAll() {
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+    ChromeOptions options = new ChromeOptions();
+
+    @BeforeEach
+
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @Test
+    void shouldReturnOkMessage() {
+        open("http://localhost:9999");
+        // Configuration.holdBrowserOpen = true;
+        $x("//*[@id=\"root\"]/div/form/fieldset/div[1]/div/span/span/span[1]/input").setValue("Москва");
+        //  $x("//a[contains(text(),'город']/..").setValue("Казань");
+        $x("//*[@id=\"root\"]/div/form/fieldset/div[2]/span/span/span/span/span[1]/input").setValue("15.11.2022");
+        $x("//input[@name='name']").setValue("Дим Димин");
+        $x("//*[@id=\"root\"]/div/form/fieldset/div[4]/span/span/span[2]/input").setValue("+70001234567");
+        //  $("[data-test-id='phone']").setValue("+70001234567");
+        $("[data-test-id='agreement']").click();
+        $x("//*[@id=\"root\"]/div/form/fieldset/div[6]/div[2]/div/button/span/span[2]").click();
+        $x("//*[@id=\"root\"]/div/div").should(visible, Duration.ofSeconds(15));
+
+    }
 }
